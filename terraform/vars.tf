3bf470f5b14a64e431ff647204861a98164c09f9
Dmitriy Khodakivsky <d.khodakivsky@whirl.sg>
2019-02-20 00:58:23 +0200

    moved vars

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,17 +1,6 @@
-variable "vsphere_server" {
-    description = "vsphere server for the environment - EXAMPLE: vcenter01.hosted.local"
-    default = "gc-vca-01.hpi.whirl.sg"
-}
- 
-variable "vsphere_user" {
-    description = "vsphere server for the environment - EXAMPLE: vsphereuser"
-    default = "LeeroyJenkins@hpi.whirl.sg"
-}
- 
-variable "vsphere_password" {
-    description = "vsphere server password for the environment"
-    default = "cPijdsa45#hnf)w"
-}
+variable "vsphere_server" {}
+variable "vsphere_user" {}
+variable "vsphere_password" {}
  
 variable "virtual_machine_dns_servers" {
   type    = "list"
