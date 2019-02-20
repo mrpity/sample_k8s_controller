46f65ba0bfb4431b4bf6afa87cd74acee00898f1
Dmitriy Khodakivsky <d.khodakivsky@whirl.sg>
2019-02-20 02:52:46 +0200

    minor refactor

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -6,31 +6,25 @@ provider "vsphere" {
   # If you have a self-signed cert
    allow_unverified_ssl = true
 }
-
 data "vsphere_datacenter" "dc" {
   name = "gc-dc-01"
 }
-
 data "vsphere_datastore" "datastore" {
   name          = "ES_E2824_LUN1"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
-
 data "vsphere_compute_cluster" "cluster" {
   name          = "gc-cl-01"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
-
 data "vsphere_resource_pool" "pool" {
   name          = "gc-cl-01/Resources/CustomerRP/WSEC_General_Test_RP"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
-
 data "vsphere_network" "network" {
   name          = "vds01-pg020"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
-
 data "vsphere_virtual_machine" "template" {
   name          = "CustomerVM/WSEC/wsec_k8s_tmplt"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
@@ -46,44 +40,48 @@ data "vsphere_virtual_machine" "template" {
 #   name          = "FIFA"
 #   parent_resource_pool_id = "${data.vsphere_resource_pool.mainpool.id}"
 # }
+#######################################################
 
-# resource "vsphere_virtual_machine" "vm" {
+resource "vsphere_virtual_machine" "vm" {
 
-#   name             = "terraform-test"
-#   resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
-#   datastore_id     = "${data.vsphere_datastore.datastore.id}"
+  name             = "terraform-test"
+  num_cpus         = 1
+  memory           = 1024
+  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+  guest_id         = "${data.vsphere_virtual_machine.template.guest_id}"
+  scsi_type        = "${data.vsphere_virtual_machine.template.scsi_type}"
+  ###############################################################
+  ## Important: Current user(Leeroy...) doesn't have permission 
+  ## to create vm's without this option.
+  folder           = "/CustomerVM/WSEC"
+  ###############################################################
 
-#   num_cpus = 2
-#   memory   = 1024
-#   guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
+  network_interface {
+    network_id = "${data.vsphere_network.network.id}"
+  }
 
-#   scsi_type = "${data.vsphere_virtual_machine.template.scsi_type}"
-
-#   network_interface {
-#     network_id = "${data.vsphere_network.network.id}"
-#   }
-
-#   disk {
-#     label = "disk0"
-#     size  = "${data.vsphere_virtual_machine.template.disks.0.size}"
-#   }
-  # clone {
-  #   template_uuid = "${data.vsphere_virtual_machine.template.id}"
+  disk {
+    label = "disk0"
+    size             = "${data.vsphere_virtual_machine.template.disks.0.size}"
+    eagerly_scrub    = "${data.vsphere_virtual_machine.template.disks.0.eagerly_scrub}"
+    thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
+  }
+  
+  clone {
+    template_uuid = "${data.vsphere_virtual_machine.template.id}"
  
-  #   customize {
-  #     linux_options {
-  #       host_name = "terraform-test"
-  #       domain    = "dev.whirl.sg"
-  #     }
- 
-  #     network_interface {
-  #       ipv4_address = "10.1.20.158"
-  #       ipv4_netmask = 24
-  #     }
- 
-  #     ipv4_gateway = "10.1.20.1"
-  #   }
-  # }
+    customize {
+      linux_options {
+        host_name = "terraform-test"
+        domain    = "dev.whirl.sg"
+      }
+      network_interface {
+        ipv4_address = "10.1.20.158"
+        ipv4_netmask = 24
+      }
+      ipv4_gateway = "10.1.20.1"
+    }
+  }
 
-
-# }
+}
