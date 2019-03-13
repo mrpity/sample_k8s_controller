ff7f659775aed5feeef8cd8733cd54ce1132faee
Dmitriy Khodakivsky <d.khodakivsky@whirl.sg>
2019-03-13 23:02:22 +0200

    separete tf files

--- terraform/create_vsphere_vm/main.tf
+++ terraform/create_vsphere_vm/main.tf
@@ -25,40 +25,23 @@ data "vsphere_network" "network" {
   name          = "${var.vsphere_network}"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
-
-########## EXAMPLE: Creating Resource Pool ###########
-# data "vsphere_resource_pool" "mainpool" {
-#   name          = "gc-cl-01/Resources/CustomerRP/"
-#   datacenter_id = "${data.vsphere_datacenter.dc.id}"
-# }
-
-# resource "vsphere_resource_pool" "main" {
-#   name          = "FIFA"
-#   parent_resource_pool_id = "${data.vsphere_resource_pool.mainpool.id}"
-# }
-#######################################################
-
 resource "vsphere_virtual_machine" "vm" {
 
-  name             = "terraform-test"
-  num_cpus         = 1
-  memory           = 1024
+  name             = "${var.vm_name}"
+  num_cpus         = "${var.vm_cpu_resources}"
+  memory           = "${var.vm_mem_resources}"
   resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
   datastore_id     = "${data.vsphere_datastore.datastore.id}"
   guest_id         = "${data.vsphere_virtual_machine.template.guest_id}"
   scsi_type        = "${data.vsphere_virtual_machine.template.scsi_type}"
-  ###############################################################
-  ## Important: Current user(Leeroy...) doesn't have permission 
-  ## to create vm's without this option.
-  folder           = "/CustomerVM/WSEC"
-  ###############################################################
+  folder           = "${var.vsphere_vm_folder}"
 
   network_interface {
     network_id = "${data.vsphere_network.network.id}"
   }
 
   disk {
-    label = "disk0"
+    label = "${var.vm_disk_label}"
     size             = "${data.vsphere_virtual_machine.template.disks.0.size}"
     eagerly_scrub    = "${data.vsphere_virtual_machine.template.disks.0.eagerly_scrub}"
     thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
@@ -69,14 +52,14 @@ resource "vsphere_virtual_machine" "vm" {
  
     customize {
       linux_options {
-        host_name = "terraform-test"
-        domain    = "dev.whirl.sg"
+        host_name = "${var.vm_customize_host_name}"
+        domain    = "${var.vm_customize_domain}"
       }
       network_interface {
-        ipv4_address = "10.1.20.158"
-        ipv4_netmask = 24
+        ipv4_address = "${var.vm_customize_network_ip}"
+        ipv4_netmask = "${var.vm_customize_network_mask}"
       }
-      ipv4_gateway = "10.1.20.1"
+      ipv4_gateway = "${var.vm_customize_network_gateway}"
     }
   }
 
--- terraform/create_vsphere_vm/variables.tf
+++ terraform/create_vsphere_vm/variables.tf
@@ -1,12 +1,10 @@
 variable "vsphere_server" {}
 variable "vsphere_user" {}
 variable "vsphere_password" {}
- 
 variable "vsphere_datacenter" {
   type = "string"
   default = "gc-dc-01"
 }
-
 variable "vsphere_datastore" {
   type = "string"
   default = "ES_E2824_LUN1"
@@ -15,19 +13,60 @@ variable "vsphere_resource_pool" {
   type = "string"
   default = "gc-cl-01/Resources/CustomerRP/WSEC_General_Test_RP"
 }
-
 variable "vsphere_virtual_machine_template" {
   type = "string"
   default = "CustomerVM/WSEC/wsec_k8s_tmplt"
 }
-
 variable "vsphere_network" {
   type = "string"
   default = "vds01-pg020"
 }
-
-
 variable "virtual_machine_dns_servers" {
   type    = "list"
   default = ["10.1.19.117", "10.1.19.118"]
+}
+
+#########################################
+# Adjust settings
+#########################################
+
+variable "vsphere_vm_fodler" {
+  type = "string"
+  default = "/CustomerVM/WSEC"
+}
+variable "vm_name" {
+  type = "string"
+  default = "terraform-test"
+}
+variable "vm_cpu_resources" {
+  type = "int"
+  default = 1
+}
+variable "vm_mem_resources" {
+  type = "int"
+  default = 1024
+}
+variable "vm_disk_label" {
+  type = "string"
+  default = "disk0"
+}
+variable "vm_customize_host_name" {
+  type = "string"
+  default = "terraform-test"
+}
+variable "vm_customize_domain" {
+  type = "string"
+  default = "dev.whirl.sg"
+}
+variable "vm_customize_network_ip" {
+  type = "string"
+  default = "10.1.20.158"
+}
+variable "vm_customize_network_mask" {
+  type = "int"
+  default = 24
+}
+variable "vm_customize_network_gateway" {
+  type = "string"
+  default = "10.1.20.1"
 }
