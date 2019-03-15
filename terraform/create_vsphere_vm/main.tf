e6f989550b3c6b0f574ed3ba403326c360e8d552
Dima Khodakivskyi <d.khodakivsky@whirl.sg>
2019-03-15 10:56:01 +0200

    refactor syntax

--- terraform/create_vsphere_vm/main.tf
+++ terraform/create_vsphere_vm/main.tf
@@ -4,29 +4,34 @@ provider "vsphere" {
   vsphere_server = "${var.vsphere_server}"
 
   # If you have a self-signed cert
-   allow_unverified_ssl = true
+  allow_unverified_ssl = true
 }
+
 data "vsphere_datacenter" "dc" {
   name = "${var.vsphere_datacenter}"
 }
+
 data "vsphere_datastore" "datastore" {
   name          = "${var.vsphere_datastore}"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
+
 data "vsphere_resource_pool" "pool" {
   name          = "${var.vsphere_resource_pool}"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
+
 data "vsphere_virtual_machine" "template" {
   name          = "${var.vsphere_virtual_machine_template}"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
+
 data "vsphere_network" "network" {
   name          = "${var.vsphere_network}"
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
-resource "vsphere_virtual_machine" "vm" {
 
+resource "vsphere_virtual_machine" "vm" {
   name             = "${var.vm_name}"
   num_cpus         = "${var.vm_cpu_resources}"
   memory           = "${var.vm_mem_resources}"
@@ -41,26 +46,27 @@ resource "vsphere_virtual_machine" "vm" {
   }
 
   disk {
-    label = "${var.vm_disk_label}"
+    label            = "${var.vm_disk_label}"
     size             = "${data.vsphere_virtual_machine.template.disks.0.size}"
     eagerly_scrub    = "${data.vsphere_virtual_machine.template.disks.0.eagerly_scrub}"
     thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
   }
-  
+
   clone {
     template_uuid = "${data.vsphere_virtual_machine.template.id}"
- 
+
     customize {
       linux_options {
         host_name = "${var.vm_customize_host_name}"
         domain    = "${var.vm_customize_domain}"
       }
+
       network_interface {
         ipv4_address = "${var.vm_customize_network_ip}"
         ipv4_netmask = "${var.vm_customize_network_mask}"
       }
+
       ipv4_gateway = "${var.vm_customize_network_gateway}"
     }
   }
-
 }
--- terraform/create_vsphere_vm/variables.tf
+++ terraform/create_vsphere_vm/variables.tf
@@ -1,26 +1,32 @@
 variable "vsphere_server" {}
 variable "vsphere_user" {}
 variable "vsphere_password" {}
+
 variable "vsphere_datacenter" {
-  type = "string"
+  type    = "string"
   default = "gc-dc-01"
 }
+
 variable "vsphere_datastore" {
-  type = "string"
+  type    = "string"
   default = "ES_E2824_LUN1"
 }
+
 variable "vsphere_resource_pool" {
-  type = "string"
+  type    = "string"
   default = "gc-cl-01/Resources/CustomerRP/WSEC_General_Test_RP"
 }
+
 variable "vsphere_virtual_machine_template" {
-  type = "string"
+  type    = "string"
   default = "CustomerVM/WSEC/wsec_k8s_tmplt"
 }
+
 variable "vsphere_network" {
-  type = "string"
+  type    = "string"
   default = "vds01-pg020"
 }
+
 variable "virtual_machine_dns_servers" {
   type    = "list"
   default = ["10.1.19.117", "10.1.19.118"]
@@ -31,42 +37,51 @@ variable "virtual_machine_dns_servers" {
 #########################################
 
 variable "vsphere_vm_folder" {
-  type = "string"
+  type    = "string"
   default = "/CustomerVM/WSEC"
 }
+
 variable "vm_name" {
-  type = "string"
+  type    = "string"
   default = "elastisearch-performance"
 }
+
 variable "vm_customize_host_name" {
-  type = "string"
+  type    = "string"
   default = "elastisearch-performance"
 }
+
 variable "vm_cpu_resources" {
-  type = "string"
+  type    = "string"
   default = "4"
 }
+
 variable "vm_mem_resources" {
-  type = "string"
+  type    = "string"
   default = "4096"
 }
+
 variable "vm_disk_label" {
-  type = "string"
+  type    = "string"
   default = "disk0"
 }
+
 variable "vm_customize_domain" {
-  type = "string"
+  type    = "string"
   default = "dev.whirl.sg"
 }
+
 variable "vm_customize_network_ip" {
-  type = "string"
+  type    = "string"
   default = "10.1.20.158"
 }
+
 variable "vm_customize_network_mask" {
-  type = "string"
+  type    = "string"
   default = "24"
 }
+
 variable "vm_customize_network_gateway" {
-  type = "string"
+  type    = "string"
   default = "10.1.20.1"
 }
