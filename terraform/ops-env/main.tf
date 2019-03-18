11bd217d39b8226ef4295bb121b3b25cda70f60c
Dima Khodakivskyi <d.khodakivsky@whirl.sg>
2019-03-18 11:01:19 +0200

    added 3 more vms

--- terraform/ops-env/main.tf
+++ terraform/ops-env/main.tf
@@ -15,3 +15,57 @@ module "vsphere-vm-rke01" {
   vsphere_user     = "${var.vsphere_user}"
   vsphere_password = "${var.vsphere_password}"
 }
+
+module "vsphere-vm-rke02" {
+  source = "../custom-modules/create_vsphere_vm"
+
+  vm_name                = "rke02.dev.whirl.sg"
+  vm_customize_host_name = "elastisearch-2"
+
+  vm_cpu_resources = "4"
+  vm_mem_resources = "4096"
+
+  vm_disk_size_1 = "50"
+
+  vm_customize_network_ip = "10.1.20.152"
+
+  vsphere_server   = "${var.vsphere_server}"
+  vsphere_user     = "${var.vsphere_user}"
+  vsphere_password = "${var.vsphere_password}"
+}
+
+module "vsphere-vm-rke03" {
+  source = "../custom-modules/create_vsphere_vm"
+
+  vm_name                = "rke03.dev.whirl.sg"
+  vm_customize_host_name = "elastisearch-3"
+
+  vm_cpu_resources = "4"
+  vm_mem_resources = "4096"
+
+  vm_disk_size_1 = "50"
+
+  vm_customize_network_ip = "10.1.20.153"
+
+  vsphere_server   = "${var.vsphere_server}"
+  vsphere_user     = "${var.vsphere_user}"
+  vsphere_password = "${var.vsphere_password}"
+}
+
+module "vsphere-vm-rke04" {
+  source = "../custom-modules/create_vsphere_vm"
+
+  vm_name                = "rke04.dev.whirl.sg"
+  vm_customize_host_name = "elastisearch-4"
+
+  vm_cpu_resources = "4"
+  vm_mem_resources = "4096"
+
+  vm_disk_size_1 = "50"
+
+  vm_customize_network_ip = "10.1.20.154"
+
+  vsphere_server   = "${var.vsphere_server}"
+  vsphere_user     = "${var.vsphere_user}"
+  vsphere_password = "${var.vsphere_password}"
+}
