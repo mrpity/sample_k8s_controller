196c40683c85efd2ac5a60e12d77b5b367fc5b62
Dima Khodakivskyi <d.khodakivsky@whirl.sg>
2019-04-09 13:28:25 +0300

    minor changes

--- terraform/aws-env/pre-prod/aws-deploy-instance/main.tf
+++ terraform/aws-env/pre-prod/aws-deploy-instance/main.tf
@@ -22,31 +22,31 @@ module "aws-key-pair" {
   AWS_SECRET_KEY = "${var.AWS_SECRET_KEY}"
 }
 
-# module "deploy-instance" {
-#     source                              = "../../../custom-modules/create_aws_ec2_with_ebs"
-#     AWS_REGION                          = "eu-west-1"
-#     INSTANCE_TYPE                       = "t2.micro"
-#     sa_security_group_id                =  "${module.aws-custom-security-group.security_group_id}"
-#     sa_key_name                         =  "${module.aws-key-pair.sa_key_name}"
-#     INSTANCE_AWS_TAG                    = "deploy-instance"
+module "deploy-instance" {
+    source                              = "../../../custom-modules/create_aws_ec2_with_ebs"
+    AWS_REGION                          = "eu-west-1"
+    INSTANCE_TYPE                       = "t2.micro"
+    sa_security_group_id                =  "${module.aws-custom-security-group.security_group_id}"
+    sa_key_name                         =  "${module.aws-key-pair.sa_key_name}"
+    INSTANCE_AWS_TAG                    = "deploy-instance"
 
 
-#     #----------------------------------------------------------------------------
-#     # https://aws.amazon.com/ec2/instance-types/
-#     # t2.micro: 1 CPU (6 credits/hour), 1 GB MEM, 8 GB DISK -- default ($0.0116)
-#     # c5.xlarge: 4 CPU, 8 GB MEM, 8 GB DISK -- default ($0.1700)
-#     #----------------------------------------------------------------------------
+    #----------------------------------------------------------------------------
+    # https://aws.amazon.com/ec2/instance-types/
+    # t2.micro: 1 CPU (6 credits/hour), 1 GB MEM, 8 GB DISK -- default ($0.0116)
+    # c5.xlarge: 4 CPU, 8 GB MEM, 8 GB DISK -- default ($0.1700)
+    #----------------------------------------------------------------------------
 
 
-#     INSTANCE_NODE_COUNT                 = "3"
+    INSTANCE_NODE_COUNT                 = "1"
 
 
-#     ADDITIONAL_EBS_DISK_SIZE            = "1"
+    ADDITIONAL_EBS_DISK_SIZE            = "1"
 
 
-#     AWS_ACCESS_KEY                      = "${var.AWS_ACCESS_KEY}"
-#     AWS_SECRET_KEY                      = "${var.AWS_SECRET_KEY}"
+    AWS_ACCESS_KEY                      = "${var.AWS_ACCESS_KEY}"
+    AWS_SECRET_KEY                      = "${var.AWS_SECRET_KEY}"
 
 
-# }
+}
 
