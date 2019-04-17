bf7352c8b3260c47aabc0b41f1b1de789e725a30
Dmitriy Khodakivsky <d.khodakivsky@whirl.sg>
2019-04-17 16:12:53 +0300

    minor changes

--- terraform/aws-env/udemy-terraform/demo-8/vars.tf
+++ terraform/aws-env/udemy-terraform/demo-8/vars.tf
@@ -2,16 +2,16 @@ variable "AWS_REGION" {
   default = "eu-west-1"
 }
 variable "PATH_TO_PRIVATE_KEY" {
-  default = "mykey"
+  default = "pity"
 }
 variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey.pub"
+  default = "pity.pub"
 }
 variable "AMIS" {
   type = "map"
   default = {
     us-east-1 = "ami-13be557e"
     us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
+    eu-west-1 = "ami-0204cddbf060b2420"
   }
 }
